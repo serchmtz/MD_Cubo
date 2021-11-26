25c22be06a13572ef9241c02379185f623f8492c
Sergio A. Martínez Fernández <salberto.mtz@gmail.com>
2021-11-26 10:40:25 -0600

    Various changes.
    * Fixed import in MD_Cubo_4x4_ICS595.cpp to work in case sentive OS.
    
    * The manipulation of low-level ports was commented on
    MD_CUBO_8X8_ICS574.CPP to compile in ESP32.

--- src/MD_Cubo_8x8_ICS574.cpp
+++ src/MD_Cubo_8x8_ICS574.cpp
@@ -14,19 +14,22 @@ void MD_Cubo_ICS574::begin(void)
   
   for(i=0; i<14; i++)
     pinMode(i, OUTPUT);
+
   
   // pinMode(A0, OUTPUT) as specified in the arduino reference didn't work. So I accessed the registers directly.
-  DDRC = 0xff;
-  PORTC = 0x00;
+
+  // This avoid compilation on esp32
+  // DDRC = 0xff;
+  // PORTC = 0x00;
   
-  // Reset any PWM configuration that the arduino may have set up automagically!
-  TCCR2A = 0x00;
-  TCCR2B = 0x00;
-  TCCR2A |= (0x01 << WGM21); // CTC mode. clear counter on TCNT2 == OCR2A
-  OCR2A = 10; // Interrupt every 25600th cpu cycle (256*100)
-  TCNT2 = 0x00; // start counting at 0
-  TCCR2B |= (0x01 << CS22) | (0x01 << CS21); // Start the clock with a 256 prescaler
-  TIMSK2 |= (0x01 << OCIE2A);
+  // // Reset any PWM configuration that the arduino may have set up automagically!
+  // TCCR2A = 0x00;
+  // TCCR2B = 0x00;
+  // TCCR2A |= (0x01 << WGM21); // CTC mode. clear counter on TCNT2 == OCR2A
+  // OCR2A = 10; // Interrupt every 25600th cpu cycle (256*100)
+  // TCNT2 = 0x00; // start counting at 0
+  // TCCR2B |= (0x01 << CS22) | (0x01 << CS21); // Start the clock with a 256 prescaler
+  // TIMSK2 |= (0x01 << OCIE2A);
 }
 
 void MD_Cubo_ICS574::animate(uint32_t wait)
@@ -65,30 +68,30 @@ uint8_t MD_Cubo_ICS574::size(axis_t axis)
 }
 
 // direct port control to the uP.
-ISR(TIMER2_COMPA_vect)
-{
-  int i;
+// ISR(TIMER2_COMPA_vect)
+// {
+//   int i;
   
-  // all layer selects off
-  PORTC = 0x00;
-  PORTB &= 0x0f;
-  PORTB |= 0x08; // output enable off.
-  for (i=0; i<8; i++) 
-  {
-    PORTD = cube[current_layer][i];
-    PORTB = (PORTB & 0xF8) | (0x07 & (i+1));
-  }
-  PORTB &= 0b00110111; // Output enable on.
-  if      (current_layer <  6) 
-    PORTC = (0x01 << current_layer); 
-  else if (current_layer == 6) 
-    digitalWrite(12, HIGH); 
-  else 
-    digitalWrite(13, HIGH);
+//   // all layer selects off
+//   PORTC = 0x00;
+//   PORTB &= 0x0f;
+//   PORTB |= 0x08; // output enable off.
+//   for (i=0; i<8; i++) 
+//   {
+//     PORTD = cube[current_layer][i];
+//     PORTB = (PORTB & 0xF8) | (0x07 & (i+1));
+//   }
+//   PORTB &= 0b00110111; // Output enable on.
+//   if      (current_layer <  6) 
+//     PORTC = (0x01 << current_layer); 
+//   else if (current_layer == 6) 
+//     digitalWrite(12, HIGH); 
+//   else 
+//     digitalWrite(13, HIGH);
   
-  current_layer++;
-  if (current_layer == 8) 
-    current_layer = 0;
-}
+//   current_layer++;
+//   if (current_layer == 8) 
+//     current_layer = 0;
+// }
 
 // end
